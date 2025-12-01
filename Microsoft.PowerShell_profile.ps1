235273e444cd9a1e53ff8a6ff24c5d30ffcc64a4
ZforCandY <zhaochengyu13@gmail.com>
2025-12-02 00:10:26 +0800

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+
+[System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;A:\IPFS\Apps\kubo_v0.38.0\kubo')
+
+[System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;C:\Users\Administrator\Apps\kubo_v0.37.0\kubo')
+Set-Alias ed edit
+Set-Alias em emacs
+Set-Alias ccl wx86cl64
+Set-Alias ef emacsclient
+# function ec
+function  ec{emacsclient -n -c -a test.el}
