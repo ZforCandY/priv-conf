7afdab2091390acb117a20f04c4114c2c89bd558
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-19 19:46:54 +0800

    Refactor pre-init.el with enhanced settings
    Updated configuration settings and added new hooks.

--- minimal-emacs.d/pre-init.el
+++ minimal-emacs.d/pre-init.el
@@ -1,14 +1,46 @@
 ;;; pre-init.el --- pre_init -*- no-byte-compile: t; lexical-binding: t; -*-
-(setq inferior-lisp-program "B:\\SBCL/sbcl.exe")
-(windmove-default-keybindings 'shift)
-(when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+;; shortcut runemacs.exe --daemon (in shell:startup)
+;; add EMACS_SERVER_FILE user var to "server file dir"
 
+;;theme,font,line
+(set-face-attribute 'default nil
+                    :height 139 :weight 'regular :width 'normal :foundry "outline" :family "Consolas")
+
+(setq mode-line-position-column-line-format '("%l:%C"))
 '(display-line-numbers-type (quote relative))
 (column-number-mode 1)
 (when (version<= "26.0.50" emacs-version)
   (global-display-line-numbers-mode))
-
 (set-face-attribute 'line-number-current-line nil
                     :foreground "#FF9E3B"
                     :weight 'bold)
+;;configs
+(setq package-install-upgrade-built-in t)
+(setq inferior-lisp-program "sbcl")
+(setq confirm-kill-emacs 'y-or-n-p)
+(add-hook 'after-init-hook #'display-time-mode)
+(add-hook 'after-init-hook #'window-divider-mode)
+(setq fast-but-imprecise-scrolling t)
+(setopt mouse-autoselect-window t)
+(setopt switch-to-buffer-obey-display-actions t)
+(setq load-prefer-newer t)
+(setq org-directory "B:\\C\\org")
+ (setq inferior-lisp-program "B:\\SBCL/sbcl.exe")
+(windmove-default-keybindings 'shift)
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+(setq server-auth-dir "C:\\Users\\Administrator\\emacs-server-auth-dir"
+      server-name "admin.txt")
+(setq treesit-font-lock-level 4)
+
+;;selected compile
+(let ((deny-list '("\\(?:[/\\\\]\\.dir-locals\\.el\\(?:\\.gz\\)?$\\)"
+                   "\\(?:[/\\\\]modus-vivendi-theme\\.el\\(?:\\.gz\\)?$\\)"
+                   "\\(?:[/\\\\][^/\\\\]+-loaddefs\\.el\\(?:\\.gz\\)?$\\)"
+                   "\\(?:[/\\\\][^/\\\\]+-autoloads\\.el\\(?:\\.gz\\)?$\\)")))
+  (setq native-comp-jit-compilation-deny-list deny-list)
+  ;; Deprecated
+  (with-no-warnings
+    (setq native-comp-deferred-compilation-deny-list deny-list)
+    (setq comp-deferred-compilation-deny-list deny-list)))
