113d751ff94dccff2d47f44756576cc575eacaf9
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-19 19:51:01 +0800

    Add files via upload

---
+++ minimal-emacs.d/custom.el
@@ -0,0 +1,17 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ace-window aggressive-indent auto-package-update buffer-terminator cape cfrs
+                compile-angel corfu grandshell-theme highlight-defined ht hydra
+                inhibit-mouse paredit pfuture tomorrow-night-deepblue-theme
+                treesit-auto vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
