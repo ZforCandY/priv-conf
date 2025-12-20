0a27261c06fa1cb17aa32ff9ea51c0a26e321175
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-20 23:52:00 +0800

    Refactor Microsoft.PowerShell_profile.ps1
    Updated PowerShell profile with new aliases and functions.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,16 @@
-
 [System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;A:\IPFS\Apps\kubo_v0.38.0\kubo')
-
 [System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;C:\Users\Administrator\Apps\kubo_v0.37.0\kubo')
-Set-Alias ed edit
-Set-Alias em emacs
-Set-Alias ccl wx86cl64
-Set-Alias ef emacsclient
 
-# function ec
-function ec{emacsclient -n -c -a test.el}
+Set-Alias e emacs
+Set-Alias ccl wx86cl64
+Set-Alias f emacsclient
+Set-Alias c clear
+
+function t{e -nw}
+function dot{f $HOME\.emacs.d}
+function ec{f -n -c -a test.el}
+
 function rs{redshift -c C:\redshift.conf -v}
 function rx{redshift -x}
+function ps1{f ~/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1}
+function org{f B:\C\org\plan.org}
