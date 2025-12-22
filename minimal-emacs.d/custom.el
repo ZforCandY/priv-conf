67905d123d3265193da5b6a0a587648d463c7dd0
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-23 03:53:20 +0800

    Update custom.el with new package selections

--- minimal-emacs.d/custom.el
+++ minimal-emacs.d/custom.el
@@ -4,11 +4,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '(     "4fd51a7e87b3d90a1b47b2b72a0b7bb48daabb708b013a250e489148d6b0532c" default))
  '(package-selected-packages
-   '(ace-window aggressive-indent auto-package-update buffer-terminator cape cfrs
-                compile-angel corfu grandshell-theme highlight-defined ht hydra
-                inhibit-mouse paredit pfuture tomorrow-night-deepblue-theme
-                treesit-auto vertico)))
+   '(aggressive-indent auto-package-update avy buffer-terminator cape cfrs
+                       compile-angel corfu ef-themes grandshell-theme
+                       highlight-defined ht hydra inhibit-mouse leuven-theme nov
+                       paredit pfuture tomorrow-night-deepblue-theme
+                       treesit-auto vertico))
+ '(package-vc-selected-packages
+   '((reader :url "https://codeberg.org/divyaranjan/emacs-reader" :make "all"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
