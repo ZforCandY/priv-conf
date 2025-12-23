9d969bfcd39a4b1aa41d2e2912647a7235fa015f
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-23 17:25:18 +0800

    Update pre-init.el

--- minimal-emacs.d/pre-init.el
+++ minimal-emacs.d/pre-init.el
@@ -1,9 +1,11 @@
 ;;; pre-init.el --- pre-init -*- no-byte-compile: t; lexical-binding: t; -*-
+
 ;; shortcut runemacs.exe --daemon (in shell:startup)
 ;; add EMACS_SERVER_FILE user var to "server file dir"
-;; Build native-compile-emacs 31 with MSYS2(UCRT64)
-;; bash scrpit for auto
-;; .dlls-add \msys2\ucrt64\bin to path
+;; Build native-compile-emacs 31 with MSYS2(UCRT64) bash-scrpit for auto
+;;(.dlls)\msys2\ucrt64\bin to path Add conpty_proxy.exe/vterm.el/vtmodule.dll to path/
+;;load-path compile setq vterm-shell"powershell"
+;;see(https://emacs-china.org/t/windows-emacs-libvterm/30140/20)
 
 ;;theme,font,line
 (set-face-attribute 'default nil
@@ -44,6 +46,7 @@
 (setq treesit-font-lock-level 4)
 ;;load-path
 (add-to-list 'load-path "C:\\Users\\Administrator\\.emacs.d\\var\\el\\emacs-reader")
+(add-to-list 'load-path "B:\\msys2\\ucrt64\\bin")
 ;;selected compile
 (let ((deny-list '("\\(?:[/\\\\]\\.dir-locals\\.el\\(?:\\.gz\\)?$\\)"
                    "\\(?:[/\\\\]modus-vivendi-theme\\.el\\(?:\\.gz\\)?$\\)"
@@ -54,3 +57,5 @@
   (with-no-warnings
     (setq native-comp-deferred-compilation-deny-list deny-list)
     (setq comp-deferred-compilation-deny-list deny-list)))
+
+
