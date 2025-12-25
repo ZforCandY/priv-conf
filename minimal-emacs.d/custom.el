1f72ea81223fa1288e53fa7259f19a6d0154ecb7
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-25 20:30:55 +0800

    Update custom.el with additional packages

--- minimal-emacs.d/custom.el
+++ minimal-emacs.d/custom.el
@@ -5,13 +5,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '(     "4fd51a7e87b3d90a1b47b2b72a0b7bb48daabb708b013a250e489148d6b0532c" default))
+   '("4fd51a7e87b3d90a1b47b2b72a0b7bb48daabb708b013a250e489148d6b0532c" default))
  '(package-selected-packages
    '(aggressive-indent auto-package-update avy buffer-terminator cape cfrs
-                       compile-angel corfu ef-themes grandshell-theme
-                       highlight-defined ht hydra inhibit-mouse leuven-theme nov
-                       paredit pfuture tomorrow-night-deepblue-theme
-                       treesit-auto vertico))
+                       compile-angel corfu dired-hacks dired-subtree ef-themes
+                       esup expreg flycheck golden-ratio grandshell-theme
+                       highlight-defined hl-line-face ht hydra inhibit-mouse
+                       leuven-theme marginalia nov orderless org-bullets paredit
+                       pfuture rainbow-delimiters reader simple-modeline
+                       tomorrow-night-deepblue-theme treesit-auto ultra-scroll
+                       vertico vterm))
  '(package-vc-selected-packages
    '((reader :url "https://codeberg.org/divyaranjan/emacs-reader" :make "all"))))
 (custom-set-faces
@@ -20,3 +23,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
