3fbecc656f67e879076634d473b6c1b7e2aed3d7
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2025-12-27 21:21:50 +0800

    Add startup time display function
    Added a function to display startup time and garbage collections.

--- minimal-emacs.d/pre-early-init.el
+++ minimal-emacs.d/pre-early-init.el
@@ -4,3 +4,12 @@
 ;;Reducing clutter(only placed in pre-early)
 (setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
+;;Startup time
+'(defun display-startup-time ()
+   "Display the startup time and number of garbage collections."
+   (message "Emacs init loaded in %.2f seconds (Full emacs-startup: %.2fs) with %d garbage collections."
+            (float-time (time-subtract after-init-time before-init-time))
+            (time-to-seconds (time-since before-init-time))
+            gcs-done))
+'(add-hook 'emacs-startup-hook #'display-startup-time 100)
