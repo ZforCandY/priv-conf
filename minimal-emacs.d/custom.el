1ad6c8d1a1c02fc62893b5a152407b4ebf9d1e72
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2026-01-03 18:41:30 +0800

    Refactor custom.el for cleaner configuration
    Updated custom variables and removed unused packages.

--- minimal-emacs.d/custom.el
+++ minimal-emacs.d/custom.el
@@ -1,27 +1,20 @@
-;;; -*- lexical-binding: t -*-
+;;; custom.el --- custom -*- no-byte-compile: t; lexical-binding: t; -*-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(4g-display-thumbnails 1)
  '(custom-safe-themes
-   '("4fd51a7e87b3d90a1b47b2b72a0b7bb48daabb708b013a250e489148d6b0532c" default))
- '(package-selected-packages
-   '(aggressive-indent auto-package-update avy buffer-terminator cape cfrs
-                       compile-angel corfu dired-hacks dired-subtree ef-themes
-                       esup expreg flycheck golden-ratio grandshell-theme
-                       highlight-defined hl-line-face ht hydra inhibit-mouse
-                       leuven-theme marginalia nov orderless org-bullets paredit
-                       pfuture rainbow-delimiters reader simple-modeline
-                       tomorrow-night-deepblue-theme treesit-auto ultra-scroll
-                       vertico vterm))
+   '("488b82a8d9ace0aea8a6825db144e3c65c4f1ef3e090b618bf311d9cdb513322"
+     "4fd51a7e87b3d90a1b47b2b72a0b7bb48daabb708b013a250e489148d6b0532c" default))
+ '(package-selected-packages nil)
  '(package-vc-selected-packages
-   '((reader :url "https://codeberg.org/divyaranjan/emacs-reader" :make "all"))))
+   '((reader :url "https://codeberg.org/divyaranjan/emacs-reader" :make "all")))
+ '(warning-suppress-types '((straight package) (lexical-binding))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-
