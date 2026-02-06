41b8aa1c18b1a8c5806d109cc53508478f5c91fb
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2026-02-06 16:33:24 +0800

    Update pre-early-init.el

--- minimal-emacs.d/pre-early-init.el
+++ minimal-emacs.d/pre-early-init.el
@@ -1,15 +1,24 @@
 ;;; pre-early-init.el --- pre_early -*- no-byte-compile: t; lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 (setq debug-on-error t)
+
+                                        ;(setq init-file-debug t)
+                                        ;(profiler-start 'cpu+mem)
 
 ;;Reducing clutter(only placed in pre-early)
 (setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
 
-;;Startup time
-'(defun display-startup-time ()
-   "Display the startup time and number of garbage collections."
-   (message "Emacs init loaded in %.2f seconds (Full emacs-startup: %.2fs) with %d garbage collections."
-            (float-time (time-subtract after-init-time before-init-time))
-            (time-to-seconds (time-since before-init-time))
-            gcs-done))
-'(add-hook 'emacs-startup-hook #'display-startup-time 100)
+                                        ;Startup time
+(defun display-startup-time ()
+  "Display the startup time and number of garbage collections."
+  (message "Emacs init loaded in %.2f seconds (Full emacs-startup: %.2fs) with %d garbage collections."
+           (float-time (time-subtract after-init-time before-init-time))
+           (time-to-seconds (time-since before-init-time))
+           gcs-done))
+(add-hook 'emacs-startup-hook #'display-startup-time 100)
+
+(provide 'pre-early-init)
+
+;;; pre-early-init.el ends here
