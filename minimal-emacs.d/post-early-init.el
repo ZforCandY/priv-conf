f886308bf2ad8a72e056fcfaf53b9ebf98f165f7
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2026-02-06 16:34:15 +0800

    Enhance post-early-init.el with commentary
    Added commentary and provide statement to post-early-init.el

--- minimal-emacs.d/post-early-init.el
+++ minimal-emacs.d/post-early-init.el
@@ -1,2 +1,8 @@
 ;;; post-early-init.el --- post_early -*- no-byte-compile: t; lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 (setq minimal-emacs-gc-cons-threshold (* 64 1024 1024))
+
+(provide 'post-early-init)
+
+;;; post-early-init.el ends here
