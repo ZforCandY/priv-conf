c7b2936d8f52dcfb24f8662186231bcea27bd350
ZforCandY <238580510+ZforCandY@users.noreply.github.com>
2026-02-10 01:45:39 +0800

    Update PowerShell profile with new aliases and functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,22 @@
 [System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;A:\IPFS\Apps\kubo_v0.38.0\kubo')
 [System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;C:\Users\Administrator\Apps\kubo_v0.37.0\kubo')
 
+oh-my-posh init pwsh --config 'negligible' | Invoke-Expression
+
 Set-Alias e emacs
 Set-Alias ccl wx86cl64
 Set-Alias f emacsclient
 Set-Alias c clear
-
+Set-Alias I timer
+Set-Alias dr drracket
+Set-Alias scp Scoop
+Set-Alias scps Scoop-search
+Set-Alias wz wezterm
 function t{e -nw}
 function dot{f $HOME\.emacs.d}
 function m{f -n -c -a x}
+function mt{B:\mintty\mintty-standalone\mintty-standalone-winpty-65001-pwsh5.cmd}
+##function mc{mintty --exec cmd //k winpty -- powershell -NoExit -Command}
 function d{e --daemon}
 
 function rs{redshift -c C:\redshift.conf -v}
