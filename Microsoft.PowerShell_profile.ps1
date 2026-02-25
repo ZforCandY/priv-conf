fe603364086b040bafa28aa5d5c345ee63b9c8ea
Forcand <238580510+ZforCandY@users.noreply.github.com>
2026-02-25 23:42:22 +0800

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
 [System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;A:\IPFS\Apps\kubo_v0.38.0\kubo')
 [System.Environment]::SetEnvironmentVariable('PATH',$Env:PATH+';;C:\Users\Administrator\Apps\kubo_v0.37.0\kubo')
 
-oh-my-posh init pwsh --config 'negligible' | Invoke-Expression
+#oh-my-posh init pwsh --config 'negligible' | Invoke-Expression
 
 Set-Alias e emacs
 Set-Alias ccl wx86cl64
@@ -12,10 +12,14 @@ Set-Alias dr drracket
 Set-Alias scp Scoop
 Set-Alias scps Scoop-search
 Set-Alias wz wezterm
+Set-Alias p umpv
+
+#function mpd {mpd C:\Users\Administrator\.mpd\mpd.conf -v}
 function t{e -nw}
 function dot{f $HOME\.emacs.d}
 function m{f -n -c -a x}
 function mt{B:\mintty\mintty-standalone\mintty-standalone-winpty-65001-pwsh5.cmd}
+function me{autohotkey C:\Users\Administrator\media.ahk}
 ##function mc{mintty --exec cmd //k winpty -- powershell -NoExit -Command}
 function d{e --daemon}
 
